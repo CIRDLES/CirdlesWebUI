5fa01abffb57e3891da12c4d6aa615d2d60df463
Jake Marotta <marottajb@g.cofc.edu>
2019-07-04 07:55:47 -0400

    Adds tabulator to topsoil page

--- src/scenes/Topsoil/index.js
+++ src/scenes/Topsoil/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { TOPSOIL_ENDPOINT } from "constants";
 import axios from "axios";
+import ReactTabulator from "react-tabulator";
 import "../../styles/topsoil.scss";
 
 class TopsoilPage extends Component {
@@ -11,11 +12,16 @@ class TopsoilPage extends Component {
     this.state = {
       selectedTableFile: null,
       template: "DEFAULT",
-      dataTable: null
+      rows: null,
+      columns: null
     };
 
     this.handleChangeTableFile = this.handleChangeTableFile.bind(this);
     this.handleChangeTemplate = this.handleChangeTemplate.bind(this);
+  }
+
+  componentDidMount() {
+
   }
 
   handleChangeTableFile(event) {
@@ -46,9 +52,12 @@ class TopsoilPage extends Component {
           }
         )
         .then(response => {
-          console.log(response);
-          this.setState({ dataTable: JSON.parse(response) });
-          console.log(this.state.dataTable);
+          const rows = response.data.data,
+                columns = response.data.columns.map(col => {
+                  col.editor = "input";
+                  return col;
+                });
+          this.setState({ rows, columns });
         })
         .catch(error => {
           console.log(error);
@@ -57,20 +66,29 @@ class TopsoilPage extends Component {
   }
 
   render() {
+    const { template, rows, columns } = this.state;
     return (
       <div id="page-container">
         <div id="upload-container">
           Select a table file (.csv, .tsv): <input type="file" name="tableFile" onChange={this.handleChangeTableFile}></input>
           <br />
           Data Template:
-          <select name="template" value={this.state.template} onChange={this.handleChangeTemplate}>
+          <select name="template" value={template} onChange={this.handleChangeTemplate}>
             <option value="DEFAULT">Default</option>
             <option value="SQUID_3">Squid 3</option>
           </select>
           <br />
           <button onClick={this.handleFileSubmission.bind(this)}>Submit</button>
         </div>
-        <div id="table-container" />
+        <div id="table-container">
+          <ReactTabulator
+            ref={ref => this.tabulator = ref}
+            columns={columns}
+            data={rows}
+            cellEdited={cell => console.log("cellEdited", cell)}
+            dataEdited={newData => console.log("dataEdited", newData)}
+          />
+        </div>
         <div id="plot-container" />
       </div>
     );
