5bb0243c1c4808a7aacd2ea3ad184c1fd1238bb2
Jake Marotta <marottajb@g.cofc.edu>
2019-07-04 22:23:49 -0400

    Replaces ReactTabulator with Tabulator

--- src/scenes/Topsoil/index.js
+++ src/scenes/Topsoil/index.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import { TOPSOIL_ENDPOINT } from "constants";
 import axios from "axios";
-import { React15Tabulator } from "react-tabulator";
+import Tabulator from "tabulator-tables";
+import "tabulator-tables"
 import "../../styles/topsoil.scss";
+import { timingSafeEqual } from "crypto";
+
+// const Tabulator = require("tabulator-tables");
+
+const testData = [
+  { label: "row1", selected: true, x: 29.165688743, y: 0.712165893, sigma_x: 0.22165688743, sigma_y: 0.00452165893, rho: 0.918191745 },
+  { label: "row2", selected: true, x: 29.031535970, y: 0.714916493, sigma_x: 0.26031535970, sigma_y: 0.004916493, rho: 0.714916493 }
+];
+
+const testColumns = [
+  { title: "Label", field: "label", editor: "input" },
+  { title: "Selected", field: "selected", editor: true, formatter: "tickCross" },
+  { title: "X", field: "x", editor: "input" },
+  { title: "σX", field: "sigma_x", editor: "input" },
+  { title: "Y", field: "y", editor: "input" },
+  { title: "σY", field: "sigma_y", editor: "input" },
+  { title: "Corr Coef", field: "rho", editor: "input" }
+]
 
 class TopsoilPage extends Component {
 
@@ -12,8 +31,8 @@ class TopsoilPage extends Component {
     this.state = {
       selectedTableFile: null,
       template: "DEFAULT",
-      rows: null,
-      columns: null
+      rows: [],
+      columns: []
     };
 
     this.handleChangeTableFile = this.handleChangeTableFile.bind(this);
@@ -21,7 +40,16 @@ class TopsoilPage extends Component {
   }
 
   componentDidMount() {
-
+    this.tabulator = new Tabulator("#tabulator", {
+      reactiveData: true,
+      data: this.state.rows,
+      columns: this.state.columns
+    });
+  }
+  
+  componentDidUpdate() {
+    this.tabulator.setData(this.state.data);
+    this.tabulator.setColumns(this.state.columns);
   }
 
   handleChangeTableFile(event) {
@@ -46,10 +74,7 @@ class TopsoilPage extends Component {
       axios
         .post(
           TOPSOIL_ENDPOINT,
-          data,
-          {
-            responseType: "application/json"
-          }
+          data
         )
         .then(response => {
           const rows = response.data.data,
@@ -66,30 +91,30 @@ class TopsoilPage extends Component {
   }
 
   render() {
-    const { template, rows, columns } = this.state;
+    const { template } = this.state;
     return (
       <div id="page-container">
         <div id="upload-container">
-          Select a table file (.csv, .tsv): <input type="file" name="tableFile" onChange={this.handleChangeTableFile}></input>
+          <div>
+            Select a table file (.csv, .tsv): <input type="file" name="tableFile" onChange={this.handleChangeTableFile}></input>
+          </div>
           <br />
-          Data Template:
-          <select name="template" value={template} onChange={this.handleChangeTemplate}>
-            <option value="DEFAULT">Default</option>
-            <option value="SQUID_3">Squid 3</option>
-          </select>
+          <div>
+            Data Template:
+            <select name="template" value={template} onChange={this.handleChangeTemplate}>
+              <option value="DEFAULT">Default</option>
+              <option value="SQUID_3">Squid 3</option>
+            </select>
+          </div>
           <br />
           <button onClick={this.handleFileSubmission.bind(this)}>Submit</button>
         </div>
         <div id="table-container">
-          <React15Tabulator
-            ref={ref => this.tabulator = ref}
-            columns={columns || []}
-            data={rows || []}
-            cellEdited={cell => console.log("cellEdited", cell)}
-            dataEdited={newData => console.log("dataEdited", newData)}
-          />
+          <div id="tabulator" />
         </div>
-        <div id="plot-container" />
+        <div id="plot-container">
+          <div id="plot" />
+        </div>
       </div>
     );
   }
