d29c2ea8001b21654b9c38058030c6fdfb86f168
Chloe Stapleton <ces@MacBook-Pro.lan>
2021-04-21 12:25:55 -0400

    put my name here too

--- src/scenes/MarsMapMaker/util/staticMapOutputText.js
+++ src/scenes/MarsMapMaker/util/staticMapOutputText.js
@@ -17,7 +17,7 @@ export const LICENSE =
   "// for the specific language governing permissions and limitations under the License.\n";
 
 export const MMM_INFO =
-  "//\n// Mars Map Maker was created by Josh Gilley and Robert Niggebrugge\n" +
+  "//\n// Mars Map Maker was created by Josh Gilley, Robert Niggebrugge, and Chloe Stapleton\n" +
   "// of CIRDLES.org, with help from James Rundle,\n// under the guidance of Principal Investigator" +
   " Dr. Jim Bowring\n// in coordination with geosamples.org.\n//\n//\n";
 
